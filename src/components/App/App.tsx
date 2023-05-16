6287a18801a9cdcde49b039a3ba17a644f402a1e
Nikita <nikitakemper555@gmail.com>
2023-05-16 14:01:14 +0300

    feat: add routing

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,7 +1,29 @@
 import './App.scss';
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
+} from 'react-router-dom';
+import MainLayout from '../../layouts/MainLayout';
+import Search from '../../pages/Search/Search';
+import Vacancy from '../../pages/Vacancy/Vacancy';
+import Favorites from '../../pages/Favorites/Favorites';
+import Notfound from '../../pages/Notfound/Notfound';
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<MainLayout />}>
+      <Route index element={<Search />} />
+      <Route path="vacancy" element={<Vacancy />} />
+      <Route path="favorites" element={<Favorites />} />
+      <Route path="*" element={<Notfound />} />
+    </Route>
+  )
+);
 
 function App() {
-  return <div className="App">App</div>;
+  return <RouterProvider router={router} />;
 }
 
 export default App;
