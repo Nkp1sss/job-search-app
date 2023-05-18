6c6fea1adfb0e10693cb541f6fda3f20124f3bab
Nikita <nikitakemper555@gmail.com>
2023-05-18 20:41:21 +0300

    feat: add auth query

--- src/redux/slices/SuperjobAPI.ts
+++ src/redux/slices/SuperjobAPI.ts
@@ -3,13 +3,24 @@ import { API_BASE_URL } from '../../constants';
 
 const superjobSlice = createApi({
   reducerPath: 'superjobSlice',
-  baseQuery: fetchBaseQuery({ baseUrl: API_BASE_URL }),
+  baseQuery: fetchBaseQuery({
+    baseUrl: API_BASE_URL,
+    headers: {
+      'x-secret-key': 'GEU4nvd3rej*jeh.eqp',
+      'X-Api-App-Id':
+        'v3.r.137440105.ffdbab114f92b821eac4e21f485343924a773131.06c3bdbb8446aeb91c35b80c42ff69eb9c457948',
+    },
+  }),
   endpoints: (builder) => ({
+    auth: builder.query({
+      query: () =>
+        `oauth2/password/?login=sergei.stralenia@gmail.com&password=paralect123&client_id=2356&client_secret=v3.r.137440105.ffdbab114f92b821eac4e21f485343924a773131.06c3bdbb8446aeb91c35b80c42ff69eb9c457948&hr=0`,
+    }),
     getCatalogues: builder.query({
       query: () => `catalogues/`,
     }),
   }),
 });
 
-export const { useGetCataloguesQuery } = superjobSlice;
+export const { useAuthQuery, useGetCataloguesQuery } = superjobSlice;
 export default superjobSlice;
