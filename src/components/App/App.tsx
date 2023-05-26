f108ec6d8b15c91fbb1cb9301132f51932684e39
Nikita <nikitakemper555@gmail.com>
2023-05-27 00:15:44 +0300

    fix

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -33,7 +33,18 @@ const router = createBrowserRouter(
 function App() {
   const getAuthFromLocalStorage = (): AuthType => {
     const authString = localStorage.getItem('auth');
-    return JSON.parse(authString as string);
+    if (authString) return JSON.parse(authString);
+    else
+      return {
+        access_token:
+          'v3.r.137440105.0229c96f0b428d9d6a015e41938bf6277e1ef7aa.59912cd729e49171c4d420cad5cb6e2b1a836b70',
+        refresh_token:
+          'v3.r.137440105.8c1f0af3195f48bcd6160a2eb499d101d893cb37.45a2f06ec9bed618f419e1be2c7bc6d50e6ad05c',
+        ttl: 1,
+        expires_in: 604800,
+        token_type: 'Bearer',
+        reg_user_resumes_count: 1,
+      };
   };
   const setAuthToLocalStorage = (data: AuthType): void => {
     localStorage.setItem('auth', JSON.stringify(data));
