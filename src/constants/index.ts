c17bffbfc1ee844d29d83ae26e54aee21090d008
Nikita <nikitakemper555@gmail.com>
2023-08-29 23:07:02 +0300

    feat: change proxy

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,4 +1,4 @@
-const API_BASE_URL = 'https://proxy-production-431b.up.railway.app/2.0/';
+const API_BASE_URL = 'https://startup-summer-2023-proxy.onrender.com/2.0/';
 
 const MAX_VACANCIES = 500;
 const COUNT_VACANCY_OF_PAGE = 4;
